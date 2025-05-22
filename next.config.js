9bfa70952f4b8e37029b6c49451edc0a4c3fde99
azmusweb <azmusweb@gmail.com>
2025-05-22 14:02:06 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['drive.google.com', 'sarjana.web.id'],
-  },
-};
-module.exports = nextConfig;
