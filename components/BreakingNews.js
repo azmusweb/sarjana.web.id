3973d642d81b593a764bcd17bce60bae67607dcf
azmusweb <azmusweb@gmail.com>
2025-05-26 08:58:19 +0700

    Delete components directory

--- components/BreakingNews.js
+++
@@ -1,8 +0,0 @@
-export default function BreakingNews({ posts }) {
-  const text = posts.slice(0, 5).map(p => p.Judul).join(" • ");
-  return (
-    <div className="w-full bg-red-500 text-white py-2 font-semibold">
-      <marquee>{text}</marquee>
-    </div>
-  );
-}
